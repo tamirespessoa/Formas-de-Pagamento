639d4becc476797f47053a5e2fec717f419baa1e
Tamires Sousa Pessoa <109546094+tamirespessoa@users.noreply.github.com>
2022-12-04 22:15:30 -0300

    Add files via upload

---
+++ quinto-desafio.js
@@ -0,0 +1,7 @@
+const produto = 50.00
+const debito = (produto * 90) / 100;
+const dinheiro = (produto * 85) / 100;
+const duasVezes = (produto) / 2;
+const acimadeduasvezes = (produto + (produto * 0.1));
+
+console.log(acimadeduasvezes);
